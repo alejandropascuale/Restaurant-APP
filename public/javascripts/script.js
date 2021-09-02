efa22b18e42323fe592e8e8cf90961d2a9b2f63f
alejandropascuale <apascuale@outlook.com>
2021-09-02 11:16:01 -0300

    cambio de puerto y paso a localhost para live server

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,5 +2,9 @@
 window.addEventListener('load', () => {
     let menu = document.querySelector('#menu-bars');
     let navbar = document.querySelector('.navbar');
-    
+  
+    menu.addEventListener('click', ()=> {
+        menu.classList.toggle('fa-times');
+        navbar.classList.toggle('active');
+    })
 })
