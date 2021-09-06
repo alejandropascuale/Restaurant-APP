c49a0b668c6d9273adb75fd6509697d272bdb0d5
alejandropascuale <apascuale@outlook.com>
2021-09-06 12:42:18 -0300

    search icon config width efects responsive

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,9 +2,24 @@
 window.addEventListener('load', () => {
     let menu = document.querySelector('#menu-bars');
     let navbar = document.querySelector('.navbar');
+    let searchForm = document.querySelector('#search-form');
+    let searchIcon = document.querySelector('#search-icon');
+    let closeIcon = document.querySelector('#close');
   
     menu.addEventListener('click', ()=> {
         menu.classList.toggle('fa-times');
         navbar.classList.toggle('active');
+    });
+
+    window.addEventListener('scroll', ()=> {
+        menu.classList.remove('fa-times');
+        navbar.classList.toggle('active');
+    });
+
+    searchIcon.addEventListener('click', () => {
+        searchForm.classList.toggle('active');
+    })
+    closeIcon.addEventListener('click', () => {
+        searchForm.classList.remove('active');
     })
 })
